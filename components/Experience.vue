9e15fe240572184c10a68f08565f5f2fb72b2086
Anand Parikh <parikhanand9898@gmail.com>
2024-09-16 13:29:54 +0530

    intial commit

---
+++ components/Experience.vue
@@ -0,0 +1,71 @@
+<template>
+  <section id="skills" class="py-16">
+    <div class="container px-4 mx-auto">
+      <h2 class="text-center text-3xl md:text-5xl font-medium mb-16">
+        Experience
+      </h2>
+
+      <div class="w-full bg-back px-4 md:px-10 py-5 flex justify-center">
+        <div
+          class="w-full lg:w-1/2 pt-8 lg:pl-4 relative"
+          style="margin-left: 15%"
+        >
+          <!-- Vertical Line -->
+          <div
+            class="absolute top-0 left-1/3 w-px bg-blue-600"
+            style="height: 100%; transform: translateX(-50%)"
+          ></div>
+          <!-- Dots -->
+          <div
+            class="absolute left-1/3 transform -translate-x-1/2 w-2 h-2 bg-blue-600 rounded-full"
+            style="top: 15%"
+          ></div>
+          <div
+            class="absolute left-1/3 transform -translate-x-1/2 w-2 h-2 bg-blue-600 rounded-full"
+            style="top: 44%"
+          ></div>
+          <div
+            class="absolute left-1/3 transform -translate-x-1/2 w-2 h-2 bg-blue-600 rounded-full"
+            style="top: 74%"
+          ></div>
+          <!-- Experiences -->
+          <div class="flex flex-col-reverse md:flex-row w-full mb-6 relative">
+            <div class="text-blue-600 mr-4 font-medium w-full md:w-1/3">
+              Jan 2022 - Present
+            </div>
+            <div class="w-full md:w-2/3 pl-10 md:pl-0">
+              <p class="font-medium mb-1 text-lg">Sr. Php Developer</p>
+              <p class="text-sm md:text-base text-gray-500">
+                Impero IT Services Pvt. Ltd.
+              </p>
+            </div>
+          </div>
+
+          <div class="flex flex-col-reverse md:flex-row w-full mb-6 relative">
+            <div class="text-blue-600 mr-4 font-medium w-full md:w-1/3">
+              May 2021 - January 2022
+            </div>
+            <div class="w-full md:w-2/3 pl-10 md:pl-0">
+              <p class="font-medium mb-1 text-lg">SDE</p>
+              <p class="text-sm md:text-base text-gray-500">
+                Brainvire Infotech Inc.
+              </p>
+            </div>
+          </div>
+
+          <div class="flex flex-col-reverse md:flex-row w-full mb-6 relative">
+            <div class="text-blue-600 mr-4 font-medium w-full md:w-1/3">
+              June 2018 - May 2021
+            </div>
+            <div class="w-full md:w-2/3 pl-10 md:pl-0">
+              <p class="font-medium mb-1 text-lg">Php Developer</p>
+              <p class="text-sm md:text-base text-gray-500">
+                TRooTech Business Solutions Pvt. Ltd.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
